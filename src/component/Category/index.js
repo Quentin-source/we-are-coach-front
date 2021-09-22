66d7c75175f02bae6361f01e440bba42d02ef0bf
Maeva Dumas <18.dumas@gmail.com>
2021-09-22 15:00:41 +0200

    carousel

--- src/component/Category/index.js
+++ src/component/Category/index.js
@@ -3,24 +3,58 @@
 import React from 'react';
 import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
 import 'pure-react-carousel/dist/react-carousel.es.css';
+import { Image } from 'semantic-ui-react';
+import motocross from '../../assets/images/motocross.jpg';
+import musculation from '../../assets/images/musculation.jpg';
+import surf from '../../assets/images/surf.jpg';
+import './style.scss';
  
 class carousel extends React.Component {
     render() {
         return (
             <CarouselProvider
-                naturalSlideWidth={100}
-                naturalSlideHeight={125}
+                naturalSlideWidth={10}
+                naturalSlideHeight={10}
                 totalSlides={3}
+                isPlaying={true}
+                interval={3000}
+                infinite={false}
+            
             >
-                <Slider>
-                    <Slide index={0}>I am the first Slide.</Slide>
-                    <Slide index={1}>I am the second Slide.</Slide>
-                    <Slide index={2}>I am the third Slide.</Slide>
-                </Slider>
+                <div className="slide">
+                    <Slider >
+                        <Slide index={0}
+                        >
+                            <Image src={motocross}/>
+                            <div className="topo">
+                                <h2 className="title">Motocross</h2>
+                                <p className="text">Essayez ce sport, il est incroyable!</p>
+                            </div>
+                        </Slide>
+                        
+                        
+                        <Slide index={1}>
+                            <Image src={musculation}/>
+                            <div className="topo">
+                                <h2 className="title">Musculation</h2>
+                                <p className="text">Essayez ce sport, il est incroyable!</p>
+                            </div>
+                        </Slide>
+                        <Slide index={2}>
+                            <Image src={surf}/>
+                            <div className="topo">
+                                <h2 className="title">Surf</h2>
+                                <p className="text">Essayez ce sport, il est incroyable!</p>
+                            </div>
+                        </Slide>
+                    </Slider>
+                </div>
                 <ButtonBack>Back</ButtonBack>
                 <ButtonNext>Next</ButtonNext>
             </CarouselProvider>
         );
+
+
     }
 }
 
