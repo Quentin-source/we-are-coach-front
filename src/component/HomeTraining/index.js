f2a1adcafbda612517ec6a325123eaff0e70aceb
Maeva Dumas <18.dumas@gmail.com>
2021-09-24 09:15:56 +0200

    newtraining

--- src/component/HomeTraining/index.js
+++ src/component/HomeTraining/index.js
@@ -14,75 +14,76 @@ const HomeTraining = () => {
         >
             <div className="container">
                 <List sx={{ width: 2000, height: 550, bgcolor: 'background.paper' }}>
-                    <ListItem  sx={{ textalign: "center" }} alignItems="flex-start">
-                        <ListItemAvatar>
-                            <Avatar  sx={{ width: 150, height: 150 }} alt="boxeur" src={boxe} />
-                        </ListItemAvatar>
-                        <ListItemText
-                            primary="Entraînement 1"
-                            secondary={
-                                <React.Fragment>
-                                    <Typography 
-                                        sx={{ display: 'inline', fontSize: 24, fontWeight: 'medium'}}
-                                        component="span"
-                                        variant="body2"
-                                        color="text.primary"
-                                    >
+                    <div className="incontainer">
+                        <ListItem  sx={{ textalign: "center" }} alignItems="flex-start">
+                            <ListItemAvatar>
+                                <Avatar  sx={{ width: 150, height: 150 }} alt="boxeur" src={boxe} />
+                            </ListItemAvatar>
+                            <ListItemText
+                                primary="Top 1 des entraînements"
+                                secondary={
+                                    <React.Fragment>
+                                        <Typography 
+                                            sx={{ display: 'inline', fontSize: 24, fontWeight: 'medium'}}
+                                            component="span"
+                                            variant="body2"
+                                            color="text.primary"
+                                        >
                                             Jean Tend
-                                    </Typography>
-                                    <div className="text">{"— Boxe, krav-maga c'est partis, pour gagner des muscles!"}
-                                    </div>
-                                </React.Fragment>
-                            }
-                        />
-                    </ListItem>
-                    <Divider variant="inset" component="li" />
-                    <ListItem alignItems="flex-start">
-                        <ListItemAvatar>
-                            <Avatar sx={{ width: 150, height: 150 }} alt="cycliste" src={velo} />
-                        </ListItemAvatar>
-                        <ListItemText
-                            sx={{ display: 'inline', fontSize: 24, fontWeight: 'medium' }}
-                            primary="Entraînement 2"
-                            secondary={
-                                <React.Fragment>
-                                    <Typography
-                                        sx={{ display: 'inline', fontSize: 24, fontWeight: 'medium', marginleft: 90 }}
-                                        component="span"
-                                        variant="body2"
-                                        color="text.primary"
-                                    >
+                                        </Typography>
+                                        <div className="text">{"— Boxe, krav-maga c'est partis, pour gagner des muscles!"}
+                                        </div>
+                                    </React.Fragment>
+                                }
+                            />
+                        </ListItem>
+                        <Divider variant="inset" component="li" />
+                        <ListItem alignItems="flex-start">
+                            <ListItemAvatar>
+                                <Avatar sx={{ width: 150, height: 150 }} alt="cycliste" src={velo} />
+                            </ListItemAvatar>
+                            <ListItemText
+                                sx={{ display: 'inline', fontSize: 24, fontWeight: 'medium' }}
+                                primary="Top 2 des entraînements"
+                                secondary={
+                                    <React.Fragment>
+                                        <Typography
+                                            sx={{ display: 'inline', fontSize: 24, fontWeight: 'medium', marginleft: 90 }}
+                                            component="span"
+                                            variant="body2"
+                                            color="text.primary"
+                                        >
                                         Alex Xandry
-                                    </Typography>
-                                    <div className="text">{" — Faire du vélo, c'est bon pour le coeur, suivez ma routine!"}</div>
-                                </React.Fragment>
-                            }
-                        />
-                    </ListItem>
-                    <Divider variant="inset" component="li" />
-                    <ListItem alignItems="flex-start">
-                        <ListItemAvatar>
-                            <Avatar sx={{ width: 150, height: 150 }} alt="muscu" src={photo} />
-                        </ListItemAvatar>
-                        <ListItemText
-                            sx={{ display: 'inline', fontSize: 24, fontWeight: 'medium' }}
-                            primary="Entraînement 3"
-                            secondary={
-                                <React.Fragment>
-                                    <Typography
-                                        sx={{ display: 'inline', fontSize: 24, fontWeight: 'medium' }}
-                                        component="span"
-                                        variant="body2"
-                                        color="text.primary"
-                                    >
+                                        </Typography>
+                                        <div className="text">{" — Faire du vélo, c'est bon pour le coeur, suivez ma routine!"}</div>
+                                    </React.Fragment>
+                                }
+                            />
+                        </ListItem>
+                        <Divider variant="inset" component="li" />
+                        <ListItem alignItems="flex-start">
+                            <ListItemAvatar>
+                                <Avatar sx={{ width: 150, height: 150 }} alt="muscu" src={photo} />
+                            </ListItemAvatar>
+                            <ListItemText
+                                sx={{ display: 'inline', fontSize: 24, fontWeight: 'medium' }}
+                                primary="Top 3 des entraînements"
+                                secondary={
+                                    <React.Fragment>
+                                        <Typography
+                                            sx={{ display: 'inline', fontSize: 24, fontWeight: 'medium' }}
+                                            component="span"
+                                            variant="body2"
+                                            color="text.primary"
+                                        >
                                         Sandie Kilos
-                                    </Typography>
-                                    <div className="text">{' — Mon entraînement va vous faire perdre vos kilos en trop.'}</div>
-                                </React.Fragment>
-                            }
-                        />
-                    </ListItem>
-                    
+                                        </Typography>
+                                        <div className="text">{' — Mon entraînement va vous faire perdre vos kilos en trop.'}</div>
+                                    </React.Fragment>
+                                }
+                            />
+                        </ListItem>
+                    </div>
                 </List>
             </div>
         </Container> 
