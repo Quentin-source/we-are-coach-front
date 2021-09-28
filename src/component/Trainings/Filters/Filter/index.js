b4d0dfa12b65ee608f82e00d6f84db944fcbba9d
thomasdesse <dessethomas@gmail.com>
2021-09-28 13:46:56 +0200

    debut filters

--- src/component/Trainings/Filters/Filter/index.js
+++ src/component/Trainings/Filters/Filter/index.js
@@ -13,7 +13,7 @@ const Filter = () => {
     return (
         <div>
             <FormControl sx={{ m: 1, minWidth: 120 }}>
-                <InputLabel id="demo-simple-select-helper-label">Age</InputLabel>
+                <InputLabel id="demo-simple-select-helper-label">Catégorie</InputLabel>
                 <Select
                     labelId="demo-simple-select-helper-label"
                     id="demo-simple-select-helper"
@@ -21,13 +21,14 @@ const Filter = () => {
                     label="Age"
                     onChange={handleChange}
                     variant="standard"
-                >
+                >                  
                     <MenuItem value="">
                         <Checkbox defaultChecked /><em>None</em>
                     </MenuItem>
-                    <MenuItem value={10}><Checkbox />Ten</MenuItem>
-                    <MenuItem value={20}><Checkbox />Twenty</MenuItem>
-                    <MenuItem value={30}><Checkbox />Thirty</MenuItem>
+                    <MenuItem value={10}><Checkbox />Catégorie</MenuItem>
+                    <MenuItem value={20}><Checkbox />Sport</MenuItem>
+                    <MenuItem value={30}><Checkbox />Difficulté</MenuItem>
+                    <MenuItem value={30}><Checkbox />Note</MenuItem>
                 </Select>
                 {/* <FormHelperText>With label + helper text</FormHelperText> */}
             </FormControl>
