d213bbd4c8b7160f6d0ad1829d88b7a6ea3c9990
Quentin-source <carlier.quentin@gmail.com>
2021-09-29 09:45:29 +0200

    formik integration ongoing

--- src/component/Header/PopUp/SignUpPop/index.js
+++ src/component/Header/PopUp/SignUpPop/index.js
@@ -1,13 +1,22 @@
 import { useDispatch, useSelector } from 'react-redux';
 
-import Button from '@mui/material/Button';
 import NavButton from '../../NavButton'
-import TextField from '@mui/material/TextField';
+
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
+
+import Checkbox from '@mui/material/Checkbox';
+import { Grid, Avatar, Typography, TextField, Button, FormControlLabel } from '@material-ui/core';
+
+import { Formik, Form, ErrorMessage, useFormik } from 'formik';
+
+import * as Yup from 'yup';
+
+import './style.scss';
+
 
 import { AssignmentIndOutlined } from '@material-ui/icons';
 
@@ -29,23 +38,35 @@ const SignUppop = () =>  {
         
     };
 
-    const handleChangeUserEmail = (event) => {
-        dispatch({
-            type: 'UPDATE_USER_EMAIL',
-            value: event.target.value
-        });
-    }
+    const signUpPopState = useSelector((state) => (state.home.signUpPop));    
 
-    const handleChangeUserPassword = (event) => {
-        dispatch({
-            type: 'UPDATE_USER_PASSWORD',
-            value: event.target.value
-        });
-    }
 
-    const signUpPopState = useSelector ((state) => (state.home.signUpPop));
-    const userEmailContent = useSelector((state) => (state.user.userEmail));
-    const userPasswordContent = useSelector((state)=>(state.user.userPassword));
+    const initialValues= {
+            Nom:'',
+            Prénom:'',
+            Âge:'',
+            Email:'',
+            Ville:'',
+            MotDepasse:'',
+            ConfirmeMotDePasse:'',
+            TermsEtConditons: false
+        },
+        onSubmit: values => {
+            alert(JSON.stringify(values, null, 2));
+        },
+    });
+
+    const validationSchema=Yup.object().shape({
+        NomPrenom: Yup.string().min(5, "C'est trop court!").required('Required'),
+        Âge: Yup.string().min(17, "Trop jeune pour t'inscrire!").max(80, "Trop vieux pour t'inscrire!").required('Required'),
+        Email: Yup.string().email('Entre un mail valide').required('Required'),
+
+    });
+
+    const headerStyle={margin:0};
+
+    
+
 
     return (
         <div>
@@ -60,108 +81,34 @@ const SignUppop = () =>  {
                     <DialogContentText>
                 Veuillez renseigner les informations suivantes...
                     </DialogContentText>
-                    <TextField
-                        autoFocus
-                        className="navbar-signpop-input"
-                        color="primary"
-                        margin="dense"
-                        type="label"
-                        id="email"
-                        label="Adresse Email"
-                        fullWidth
-                        variant="standard"
-                        value={userEmailContent}
-                        onChange={handleChangeUserEmail}
-                    />
-                    <div className="navbar-signpop-container-width">
-                        <TextField
-                            className="navbar-signpop-input"
-                            autoFocus
-                            color="primary"
-                            margin="dense"
-                            id="firstname"
-                            label="Prénom"
-                            type="label"
-                            fullWidth
-                            variant="standard"
-                            value={userEmailContent}
-                            onChange={handleChangeUserEmail}
-                        />
-                        <TextField
-                            className="navbar-signpop-input"
-                            color="primary"
-                            margin="dense"
-                            id="lastname"
-                            label="Nom"
-                            fullWidth
-                            variant="standard"
-                            value={userPasswordContent}
-                            onChange={handleChangeUserPassword}
-                        />
-                    </div>
-                    <TextField
-                        className="navbar-signpop-input"
-                        autoFocus
-                        color="primary"
-                        margin="dense"
-                        id="name"
-                        label="Addresse"
-                        type="email"
-                        fullWidth
-                        variant="standard"
-                        value={userEmailContent}
-                        onChange={handleChangeUserEmail}
-                    />
-                    <TextField
-                        className="navbar-signpop-input"
-                        color="primary"
-                        margin="dense"
-                        id="name"
-                        label="Addresse"
-                        type="email"
-                        fullWidth
-                        variant="standard"
-                        value={userEmailContent}
-                        onChange={handleChangeUserEmail}
-                    />
-                    <TextField
-                        className="navbar-signpop-input"
-                        color="primary"
-                        margin="dense"
-                        id="name"
-                        label="Addresse"
-                        type="email"
-                        fullWidth
-                        variant="standard"
-                        value={userEmailContent}
-                        onChange={handleChangeUserEmail}
-                    />
-                    <TextField
-                        className="navbar-signpop-input"
-                        color="primary"
-                        margin="dense"
-                        id="name"
-                        label="Addresse"
-                        type="email"
-                        fullWidth
-                        variant="standard"
-                        value={userEmailContent}
-                        onChange={handleChangeUserEmail}
-                    />
-                    <TextField
-                        className="navbar-signpop-input"
-                        color="primary"
-                        margin="dense"
-                        id="name"
-                        label="Addresse"
-                        type="email"
-                        fullWidth
-                        variant="standard"
-                        value={userEmailContent}
-                        onChange={handleChangeUserEmail}
-                    />
-                   
-
+                    <Grid align='center'>
+                        <h2 style={headerStyle}>S'inscrire</h2>
+                        <Typography variant='caption' gutterBottom>Créer votre compte</Typography>
+                        
+                        <Avatar alt="Nom Prénom"
+                            src="/static/images/avatar/1.jpg"
+                            sx={{ width: 60, height: 60 }}>
+                        
+                        </Avatar>
+                    </Grid>
+                    <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={onSubmit}>
+                        {(props)=>(
+                            <Form>
+                                <TextField  fullWidth name='Nom Prénom' label='Nom Prénom' placeholder='Entre ton Nom et Prénom' helperText={<ErrorMessage name='NomPrenom' />} />
+                                <TextField  fullWidth name='Âge' label='Votre Âge' placeholder='Entre ton âge'
+                                    helperText={<ErrorMessage name='Âge' />}/>
+                                <TextField  fullWidth name='Email' label='Votre Email' placeholder='Entre ton adresse mail'
+                                    helperText={<ErrorMessage name='Email' />}  />
+                                <TextField  fullWidth name='Ville' label='Votre Ville' />
+                                <TextField  fullWidth name='MotDePasse' label='Mot de Passe' />
+                                <TextField  fullWidth name='ConfirmeMotDePasse' label='Confirmer Mot de Passe' />
+                        
+                                <FormControlLabel control={<TextField as={Checkbox} defaultChecked />} name='TermsEtConditions' label="J'accepte les termes et conditions d'utilisation" />
+                                <Button type="submit" variant="outlined" color="primary">S'Inscrire</Button>
+        
+                            </Form>
+                        )}
+                    </Formik>
                 </DialogContent>
                 <DialogActions>
                     <Button onClick={handleClose}>Annuler</Button>
@@ -173,3 +120,7 @@ const SignUppop = () =>  {
 }
 
 export default SignUppop;
+
+
+
+
