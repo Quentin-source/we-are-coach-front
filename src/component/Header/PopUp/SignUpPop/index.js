979549ff2472560e183e30ba50ace02ed5ed6e6d
Quentin-source <carlier.quentin@gmail.com>
2021-09-30 10:16:56 +0200

    merge signup first version

--- src/component/Header/PopUp/SignUpPop/index.js
+++ src/component/Header/PopUp/SignUpPop/index.js
@@ -3,7 +3,7 @@ import * as yup from 'yup';
 
 import { useDispatch, useSelector } from 'react-redux';
 
-import NavButton from '../../NavButton'
+import NavButton from '../../NavButton/index';
 
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
@@ -11,23 +11,43 @@ import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 
-import { TextField, Button, Grid, Avatar, Typography } from '@mui/material';
+import { TextField, Button, Grid, Avatar, Typography, FormControlLabel, Checkbox } from '@mui/material';
 
 
 
 import { AssignmentIndOutlined } from '@material-ui/icons';
 
+import './style.scss';
+
 
 
 const validationSchema = yup.object({
+    name: yup
+        .string('Entre ton nom')
+        .min(2, 'Tape ton nom')
+        .required('Nom requis'),
+    firstname: yup
+        .string('Entre ton prénom')
+        .min(2, 'Tape ton prénom')
+        .required('Prénom requis'),
     email: yup
-        .string('Enter your email')
-        .email('Enter a valid email')
-        .required('Email is required'),
+        .string('Entre ton email')
+        .email('Entre un email valide')
+        .required('Un email est requis'),
+    age: yup
+        .string('Entre ton âge')
+        .required('Age requis'),
+    city: yup
+        .string('Entre ta ville')
+        .required('Localisation requis'),
     password: yup
-        .string('Enter your password')
-        .min(8, 'Password should be of minimum 8 characters length')
-        .required('Password is required'),
+        .string('Entre ton mot de passe')
+        .min(8, 'Ton mot de passe doit être supérieur à 8 caractères')
+        .required('Un mot de passe est requis'),
+    confirmPassword: yup
+        .string('Confirme ton mot de passe')
+        .min(8, 'Ton mot de passe doit être supérieur à 8 caractères')
+        .required('Un mot de passe est requis'),
 });
 
 const SignUpPop = () => {
@@ -53,6 +73,7 @@ const SignUpPop = () => {
         initialValues: {
             email: 'foobar@example.com',
             password: 'foobar',
+            confirmPassword: 'foobar',
         },
         validationSchema: validationSchema,
         onSubmit: (values) => {
@@ -90,6 +111,50 @@ const SignUpPop = () => {
                         <TextField 
                             fullWidth
                             variant="standard"
+                            id="name"
+                            name="name"
+                            label="Nom"
+                            value={formik.values.name}
+                            onChange={formik.handleChange}
+                            error={formik.touched.name && Boolean(formik.errors.name)}
+                            helperText={formik.touched.name && formik.errors.name}
+                        />
+                        <TextField 
+                            fullWidth
+                            variant="standard"
+                            id="firstname"
+                            name="firstname"
+                            label="Prénom"
+                            value={formik.values.firstname}
+                            onChange={formik.handleChange}
+                            error={formik.touched.firstname && Boolean(formik.errors.firstname)}
+                            helperText={formik.touched.firstname && formik.errors.firstname}
+                        />
+                        <TextField 
+                            fullWidth
+                            variant="standard"
+                            id="age"
+                            name="age"
+                            label="Age"
+                            value={formik.values.age}
+                            onChange={formik.handleChange}
+                            error={formik.touched.age && Boolean(formik.errors.age)}
+                            helperText={formik.touched.age && formik.errors.age}
+                        />
+                        <TextField 
+                            fullWidth
+                            variant="standard"
+                            id="city"
+                            name="city"
+                            label="Ville"
+                            value={formik.values.city}
+                            onChange={formik.handleChange}
+                            error={formik.touched.city && Boolean(formik.errors.city)}
+                            helperText={formik.touched.city && formik.errors.city}
+                        />
+                        <TextField 
+                            fullWidth
+                            variant="standard"
                             id="email"
                             name="email"
                             label="Email"
@@ -103,16 +168,42 @@ const SignUpPop = () => {
                             fullWidth
                             id="password"
                             name="password"
-                            label="Password"
+                            label="Mot de Passe"
                             type="password"
                             value={formik.values.password}
                             onChange={formik.handleChange}
                             error={formik.touched.password && Boolean(formik.errors.password)}
                             helperText={formik.touched.password && formik.errors.password}
                         />
+                        <TextField 
+                            variant="standard"
+                            fullWidth
+                            id="confirmPassword"
+                            name="confirmPassword"
+                            label="Confirmation du Mot de Passe"
+                            type="confirmPassword"
+                            value={formik.values.confirmPassword}
+                            onChange={formik.handleChange}
+                            error={formik.touched.confirmPassword && Boolean(formik.errors.confirmPassword)}
+                            helperText={formik.touched.confirmPassword && formik.errors.confirmPassword}
+                        />
+                        <TextField
+                            id="outlined-multiline-static"
+                            label="Sports Pratiqués"
+                            multiline
+                            rows={4}
+                            defaultValue="Ecris tes sports"
+                        />
+                        <FormControlLabel 
+                            control={<Checkbox defaultChecked />} 
+                            name='TermsEtConditions' 
+                            label="J'accepte les termes et conditions d'utilisation"
+                            type="checkbox"
+                        />
                         <DialogActions>
                             <Button onClick={handleClose}>Annuler</Button>
-                            <Button color="primary" type="submit">Envoyer</Button>
+                            <Button color="primary" type="submit">Créer votre compte</Button>
+                            <Button color="primary" type="submit">J'ai déjà un compte</Button>
                         </DialogActions>
                     </form>
                 </DialogContent>
@@ -125,73 +216,4 @@ const SignUpPop = () => {
 
 export default SignUpPop;
 
-
-// import { Grid, Paper, Avatar, Typography, TextField, Button, FormControlLabel } from '@material-ui/core';
-// import Checkbox from '@mui/material/Checkbox';
-// import * as Yup from 'yup';
-
-// import './style.scss';
-
-
-
-// const SignUp = () => {
-//     const paperStyle={padding:'30px 20px', width:300, margin:'20px auto'}
-//     const headerStyle={margin:0}
-//     const initialValues={
-//         Nom:'',
-//         Prénom:'',
-//         Âge:'',
-//         Email:'',
-//         Ville:'',
-//         MotDepasse:'',
-//         ConfirmeMotDePasse:'',
-//         TermsEtConditons: false
-//     }
-//     const validationSchema=Yup.object().shape({
-//         NomPrenom: Yup.string().min(5, "C'est trop court!").required('Required'),
-//         Âge: Yup.string().min(17, "Trop jeune pour t'inscrire!").max(80, "Trop vieux pour t'inscrire!").required('Required'),
-//         Email: Yup.string().email('Entre un mail valide').required('Required'),
-
-//     })
-//     const onSubmit=(values,props)=>{
-//         console.log(values)
-//     }
-
-//     return (
-//         <Grid>
-//             <Paper elevation={20} style={paperStyle} >
-//                 <Grid align='center'>
-//                     <h2 style={headerStyle}>S'inscrire</h2>
-//                     <Typography variant='caption' gutterBottom>Créer votre compte</Typography>
-                    
-//                     <Avatar alt="Nom Prénom"
-//                         src="/static/images/avatar/1.jpg"
-//                         sx={{ width: 60, height: 60 }}>
-                    
-//                     </Avatar>
-//                 </Grid>
-//                 <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={onSubmit}>
-//                     {(props)=>(
-//                         <Form>
-//                             <Field as={TextField} fullWidth name='Nom Prénom' label='Nom Prénom' placeholder='Entre ton Nom et Prénom' helperText={<ErrorMessage name='NomPrenom' />} />
-//                             <Field as={TextField} fullWidth name='Âge' label='Votre Âge' placeholder='Entre ton âge'
-//                                 helperText={<ErrorMessage name='Âge' />}/>
-//                             <Field as={TextField} fullWidth name='Email' label='Votre Email' placeholder='Entre ton adresse mail'
-//                                 helperText={<ErrorMessage name='Email' />}  />
-//                             <Field as={TextField} fullWidth name='Ville' label='Votre Ville' />
-//                             <Field as={TextField} fullWidth name='MotDePasse' label='Mot de Passe' />
-//                             <Field as={TextField} fullWidth name='ConfirmeMotDePasse' label='Confirmer Mot de Passe' />
-                    
-//                             <FormControlLabel control={<Field as={Checkbox} defaultChecked />} name='TermsEtConditions' label="J'accepte les termes et conditions d'utilisation" />
-//                             <Button type="submit" variant="outlined" color="primary">S'Inscrire</Button>
-    
-//                         </Form>
-//                     )}
-//                 </Formik>
-//             </Paper>
-//         </Grid>
-//     )
-// }
-
-// export default SignUp;
-
+// 
