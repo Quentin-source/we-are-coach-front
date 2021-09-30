20f7b1db43a4589b15b974b15f54ee2f072139f4
Maeva Dumas <18.dumas@gmail.com>
2021-09-30 16:00:45 +0200

    new-contact

--- src/component/Contact/index.js
+++ src/component/Contact/index.js
@@ -1,4 +1,4 @@
-import { Typography, Card, CardContent, Grid, TextField, Button } from '@mui/material';
+import { Typography, Card, CardContent, Grid, TextField, Button, FormControl, Select, MenuItem, InputLabel } from '@mui/material';
 
 import './style.scss';
 
@@ -6,16 +6,29 @@ const formContact = () => {
 
     return (
         <div className="contact">
-            <Typography style={{maxWidth:450, margin:"0 auto", padding:"50px 5px"}} gutterBottom variant="h2" align="center">Contact </Typography>
+            <Typography style={{maxWidth:500, margin:"0 auto", padding:"50px 5px"}} gutterBottom variant="h2" align="center">Contact </Typography>
 
             
-            <CardContent style={{maxWidth:450, margin:"0 auto", padding:"20px 5px"}}>
+            <CardContent style={{maxWidth:500, margin:"0 auto", padding:"20px 5px"}}>
                 <Card>
                     <Typography gutterBottom variant="h5">Formulaire de Contact</Typography>
                     <Typography gutterBottom color="textSecondary" variant="body2" component="p">Besoin d'un renseignement ? Remplissez le formulaire ci-dessous et nous vous répondrons dans les meilleurs délais.</Typography>
                     <form>
                         <Grid container spacing={1}>
+                            <Grid xs={12} sm ={18} item>
+                                <FormControl fullWidth>
+                                    <InputLabel>Civilité</InputLabel>
+                                    <Select
+                                        label="Titre"
+                                        lavariant="outlined" 
+                                        fullWidth required
+                                    >
 
+                                        <MenuItem value={0}>Monsieur</MenuItem>
+                                        <MenuItem value={1}>Madame</MenuItem>
+                                    </Select>
+                                </FormControl>
+                            </Grid>
                             <Grid xs={12} sm ={6} item>
                                 <TextField label="Nom" placeholder="Entre ton nom" variant="outlined" color="primary" fullWidth required/>
                             </Grid>
