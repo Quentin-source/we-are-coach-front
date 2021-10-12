826f34a1d9cc81a8c05f580f353c7d670283bc7f
thomasdesse <dessethomas@gmail.com>
2021-10-12 13:32:27 +0200

    test

---
+++ src/tests/reducer/home.test.js
@@ -0,0 +1,9 @@
+//import homeReducer from './src/reducers/home';
+
+//describe('le reducer home', () => {
+    //describe('Structure', () => {
+        //test('il devrait s\'agir d\'une fonction', () => {
+            //expect(typeof homeReducer).toBe('function');
+        //});
+    //});
+//});
